5497cdfb65c10c90bc455767dc98b95190d3619a
Nataliia Hirniak <gn.natalia19@gmail.com>
2023-04-28 19:43:18 +0100

    [#4] Add styles config

---
+++ components/Providers.tsx
@@ -0,0 +1,16 @@
+import { ThemeProvider } from "next-themes";
+import { FC, ReactNode } from "react";
+
+interface ProvidersProps {
+  children: ReactNode;
+}
+
+const Providers: FC<ProvidersProps> = ({ children }) => {
+  return (
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      {children}
+    </ThemeProvider>
+  );
+};
+
+export default Providers;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,37 @@
 /** @type {import('tailwindcss').Config} */
+const { fontFamily } = require("tailwindcss/defaultTheme");
 module.exports = {
+  darkMode: "class",
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      primary: "#29a385",
+      title: "#fff",
+      text: "#ebebeb",
+      bg: "#212730",
+      bgAlt: "#30353b",
+      container: "#1d222a",
+      border: "#a4a6a8",
+      firstGrad: "linear-gradient(0deg, bgAlt 0%, bg 100%)",
+      secondGrad: "linear-gradient(180deg, bgAlt 0%, bg 100%)",
+      thirdGrad: "linear-gradient(180deg, bg0%, bg 100%)",
+    },
+    fontFamily: {
+      primary: ["var(--font-jost)", ...fontFamily.sans],
+      secondary: ["var(--font-caveat)", ...fontFamily.sans],
+    },
+    extend: {
+      shadow: {
+        shadow: "5px 5px rgb(255, 255, 255 /10%)",
+      },
+      transition: {
+        transition: "cubic-bezier(0.3, 0, 0.3, 1)",
+      },
+    },
   },
   plugins: [],
 };
